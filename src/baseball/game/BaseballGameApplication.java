734312e6531778697abe86f37060a5893c7aeae9
SungWook Choi <choisw950928@naver.com>
2022-02-04 12:29:20 +0900

    <Number_Baseball_Game(수정사항 진행 해야 함)> 22.02.04

--- src/baseball/game/BaseballGameApplication.java
+++ src/baseball/game/BaseballGameApplication.java
@@ -1,14 +1,12 @@
 package baseball.game;
 
-public class StartBaseballGame {
+public class BaseballGameApplication {
     public static void main(String[] args) {
         try {
             CenterProcess centerProcess = new CenterProcess();
             centerProcess.repeatBaseballGame();
         } catch (IllegalArgumentException msg) {
             System.out.println(msg.getMessage());
-        } catch (Exception e) {
-            System.out.println("정상 종료");
         }
     }
 
