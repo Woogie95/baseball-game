f8c5f3f15a6e9bcc907f97fcd3f853148c1f2a19
Woogie95 <choisw950928@gmail.com>
2022-04-16 15:43:22 +0900

    test : 인풋 예외 처리 수정

--- src/baseball/game/Input.java
+++ src/baseball/game/Input.java
@@ -4,46 +4,10 @@ import java.util.Scanner;
 
 public class Input {
 
-    private static final int BLANK_TEXT = 0;
-    private static final int MIN_ENTER_DEFAULT_VALUE = 3;
-    private static final int FIRST_DUPLICATE_VALUE_CHECK = 0;
-    private static final int SECOND_DUPLICATE_VALUE_CHECK = 1;
-    private static final int THIRD_DUPLICATE_VALUE_CHECK = 2;
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public String inputNumber() {
-        String word = SCANNER.nextLine();
-        validateCheckTest(word);
-        return word;
-
-    }
-
-    private void validateCheckTest(String word) {
-        validateBlank(word);
-        validateNumberLength(word);
-        validateCheckText(word);
-
-    }
-
-    private void validateBlank(String word) {
-        if (word.length() == BLANK_TEXT) {
-            throw new IllegalArgumentException("아무것도 입력하지 않았습니다.");
-        }
-
-    }
-
-    private void validateNumberLength(String word) {
-        if (word.length() != MIN_ENTER_DEFAULT_VALUE) {
-            throw new IllegalArgumentException("입력해야 할 숫자 개수가 다릅니다.");
-        }
-    }
-
-    private void validateCheckText(String word) {
-        char[] ch = word.toCharArray();
-        if (ch[FIRST_DUPLICATE_VALUE_CHECK] == ch[SECOND_DUPLICATE_VALUE_CHECK] || ch[FIRST_DUPLICATE_VALUE_CHECK] ==
-                ch[THIRD_DUPLICATE_VALUE_CHECK] || ch[SECOND_DUPLICATE_VALUE_CHECK] == ch[THIRD_DUPLICATE_VALUE_CHECK]) {
-            throw new IllegalArgumentException("중복 입력 또는 음수 입니다.");
-        }
+        return SCANNER.nextLine();
     }
 
 }
--- src/baseball/game/UserNumber.java
+++ src/baseball/game/UserNumber.java
@@ -8,11 +8,16 @@ import static baseball.game.CenterProcess.MAX_VALUE;
 public class UserNumber {
 
     public static final int DEFAULT = 0;
+    private static final int BLANK_TEXT = 0;
+    private static final int MIN_DEFAULT_VALUE = 3;
+    private static final int FIRST_DUPLICATE_VALUE_CHECK = 0;
+    private static final int SECOND_DUPLICATE_VALUE_CHECK = 1;
+    private static final int THIRD_DUPLICATE_VALUE_CHECK = 2;
 
     public List<Integer> makeUserNumber() {
         List<Integer> userNumber = new ArrayList<>();
         Input input = new Input();
-        int[] number = changeText(input.inputNumber());
+        int[] number = changeText(validateCheckTest(input.inputNumber()));
 
         for (int i = DEFAULT; i < MAX_VALUE; i++) {
             userNumber.add(number[i]);
@@ -21,18 +26,44 @@ public class UserNumber {
     }
 
     private int[] changeText(String word) {
-        String[] textArr = new String[3];
-        int[] changeArr = new int[3];
-        try {
-            for (int i = 0; i < word.length(); i++) {
-                textArr[i] = String.valueOf(word.charAt(i));
+        String[] textArr = new String[MIN_DEFAULT_VALUE];
+        int[] changeArr = new int[MIN_DEFAULT_VALUE];
+
+            for (int i = DEFAULT; i < word.length(); i++) {
+                textArr[i] = String.valueOf((word.charAt(i)));
             }
-            for (int i = 0; i < word.length(); i++) {
+            for (int i = DEFAULT; i < word.length(); i++) {
                 changeArr[i] = Integer.parseInt(textArr[i]);
-            }
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
         }
         return changeArr;
+    }
+
+    private String validateCheckTest(String word) {
+        validateBlank(word);
+        validateNumberLength(word);
+        validateCheckText(word);
+
+        return word;
+    }
+
+    private void validateBlank(String word) {
+        if (word.length() == BLANK_TEXT) {
+            throw new IllegalArgumentException("아무것도 입력하지 않았습니다.");
+        }
+
+    }
+
+    private void validateNumberLength(String word) {
+        if (word.length() != MIN_DEFAULT_VALUE) {
+            throw new IllegalArgumentException("입력해야 할 숫자 개수가 다릅니다.");
+        }
+    }
+
+    private void validateCheckText(String word) {
+        char[] ch = word.toCharArray();
+        if (ch[FIRST_DUPLICATE_VALUE_CHECK] == ch[SECOND_DUPLICATE_VALUE_CHECK] || ch[FIRST_DUPLICATE_VALUE_CHECK] ==
+                ch[THIRD_DUPLICATE_VALUE_CHECK] || ch[SECOND_DUPLICATE_VALUE_CHECK] == ch[THIRD_DUPLICATE_VALUE_CHECK]) {
+            throw new IllegalArgumentException("중복 입력 입니다.");
+        }
     }
 }
